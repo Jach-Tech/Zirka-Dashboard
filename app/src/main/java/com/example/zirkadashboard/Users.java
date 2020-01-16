17cc38998652a1485fc092c58b5f856a6b2c2fd8
Jacob Limbi <jacob@jachtechnologies.com>
2020-01-16 15:55:21 +0100

    update sur le dashboard

--- app/src/main/java/com/example/zirkadashboard/Users.java
+++ app/src/main/java/com/example/zirkadashboard/Users.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Users extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_earnings);
+        setContentView(R.layout.activity_users);
     }
 }
